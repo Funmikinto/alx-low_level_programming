351f845aeb24bd235a5e424eba4eab1015112710
Funmikinto <oluwafunmitotaiwo123@gmail.com>
2023-03-16 19:15:20 -0700

    Task 7 done

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - This is the main function
+ *
+ * Return: the return is alwasy 0
+ */
+
+int main(void)
+{
+	printf("Size of a char: %lu byte(s)", sizeof(char)/n);
+	printf("Size of an int: %lu byte(s)", sizeof(int)/n);
+	printf("Size of a long int: %lu byte(s)", sizeof(long int)/n);
+	printf("Size of a long long int: %lu byte(s)", sizeof(long long int)/n);
+	printf("Size of a float: %lu byte(s)", sizeof(float)/n);
+	return (0);
+}
+
+
+
