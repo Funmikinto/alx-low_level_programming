18a9a2927f171d451717c6ee86542bb4c489f8f7
Funmikinto <oluwafunmitotaiwo123@gmail.com>
2023-03-19 14:23:11 -0700

    Task 8 done

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+/**
+ * main - main function
+ *
+ * Return: always 0
+ */
+
+#include <stdio.h>
+int main(void)
+{
+	char i;
+
+	for (i = 'z' ; i >= 'a' ; i--)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
