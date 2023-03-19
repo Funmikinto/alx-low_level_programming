561730226dc687b9408bbf8355d49b25f8855f94
Funmikinto <oluwafunmitotaiwo123@gmail.com>
2023-03-19 13:37:10 -0700

    Task 4 done

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+/**
+ * main - main function
+ *
+ * Return: always 0
+ */
+
+#include <stdio.h>
+int main(void)
+{
+	char i;
+
+	for (i = 'a' ; i <= 'z' ; i++)
+		putchar(i);
+	for (i = 'A' ; i <= 'Z' ; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
+
+
