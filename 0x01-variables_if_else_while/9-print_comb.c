989284f081698157f8cafbede4709923a7bcb0d5
Funmikinto <oluwafunmitotaiwo123@gmail.com>
2023-03-19 14:58:11 -0700

    Task 10 done

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+/**
+ * main - main functionn
+ *
+ * Return: always 0
+ */
+
+#include <stdio.h>
+int main(void)
+{
+	int i;
+
+	for (i = 0 ; i < 10 ; i++)
+	{
+		if (i == 9)
+			putchar(i + '0');
+		else
+		{
+			putchar(i + '0');
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	return (0);
+}
