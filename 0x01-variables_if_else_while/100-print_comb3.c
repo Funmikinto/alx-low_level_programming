abd5e93455187ba3ffc8d3f3fe8ef60da1d22b47
Funmikinto <oluwafunmitotaiwo123@gmail.com>
2023-03-19 17:04:24 -0700

    Task 11 done

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,32 @@
+/**
+ * main - main funtion
+ *
+ * Return: always 0
+ */
+
+#include <stdio.h>
+int main(void)
+{
+	int i;
+	int m;
+
+	for (i = 0 ; i < 10 ; i++)
+	{
+		for (m = 1 ; m < 10 ; m++)
+		{
+			if (i < m && i != m)
+			{
+				putchar(i + '0');
+				putchar(m + '0');
+				if (i + m != 17)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
+
