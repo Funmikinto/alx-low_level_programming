744cf2c065b0b1b7e2f74f51dee722db5ca00567
Funmikinto <oluwafunmitotaiwo123@gmail.com>
2023-03-19 18:00:36 -0700

    Last task done

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -19,11 +19,9 @@ int main(void)
 			putchar((j / 10) + '0');
 			putchar((j % 10) + '0');
 			if (i == 98 && j == 99)
-			{
-				continue;
-				putchar(',');
-				putchar(' ');
-			}
+			continue;
+			putchar(',');
+			putchar(' ');
 		}
 	}
 	putchar('\n');
