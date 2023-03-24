598a907c2c432b7cdc75465a656f7c8db14837ae
Funmikinto <oluwafunmitotaiwo123@gmail.com>
2023-03-24 15:42:48 -0700

    Task 3 done

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * nul - multiplies two integers
+ * @x: the first number to multiply
+ * @y: The second number to multiply with the first number
+ * Return: the result of multiplication between x and y
+ */
+
+int nul(int x, int y)
+{
+	return (x * y);
+}
